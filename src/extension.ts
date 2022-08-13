32ca6ec829db57bb432639c42c61cb1cb63f8dd0
Patrick White <1275985+packysauce@users.noreply.github.com>
2022-08-12 21:09:31 -0400

    formatting

--- src/extension.ts
+++ src/extension.ts
@@ -4,13 +4,13 @@ import { VTaskProvider } from './tasks';
 import { activateVls, deactivateVls, isVlsEnabled } from './langserver';
 
 const cmds = {
-    'v.run': commands.run,
-    'v.fmt': commands.fmt,
-    'v.ver': commands.ver,
-    'v.prod': commands.prod,
-    'v.devbits_playground': commands.devbitsPlayground,
-    'v.vls.update': commands.updateVls,
-    'v.vls.restart': commands.restartVls,
+	'v.run': commands.run,
+	'v.fmt': commands.fmt,
+	'v.ver': commands.ver,
+	'v.prod': commands.prod,
+	'v.devbits_playground': commands.devbitsPlayground,
+	'v.vls.update': commands.updateVls,
+	'v.vls.restart': commands.restartVls,
 };
 
 /**
@@ -18,39 +18,39 @@ const cmds = {
  * @param context The extension context
  */
 export function activate(context: ExtensionContext): void {
-    vscode.tasks.registerTaskProvider("v", new VTaskProvider)
+	vscode.tasks.registerTaskProvider("v", new VTaskProvider)
 
-    for (const cmd in cmds) {
-        const handler = cmds[cmd] as () => void;
-        const disposable = vscode.commands.registerCommand(cmd, handler);
-        context.subscriptions.push(disposable);
-    }
+	for (const cmd in cmds) {
+		const handler = cmds[cmd] as () => void;
+		const disposable = vscode.commands.registerCommand(cmd, handler);
+		context.subscriptions.push(disposable);
+	}
 
-    workspace.onDidChangeConfiguration((e: ConfigurationChangeEvent) => {
-        if (e.affectsConfiguration('v.vls.enable')) {
-            if (isVlsEnabled()) {
-                void activateVls();
-            } else {
-                deactivateVls();
-            }
-        } else if (e.affectsConfiguration('v.vls') && isVlsEnabled()) {
-            void vscode.window.showInformationMessage('VLS: Restart is required for changes to take effect. Would you like to proceed?', 'Yes', 'No')
-                .then(selected => {
-                    if (selected == 'Yes') {
-                        void vscode.commands.executeCommand('v.vls.restart');
-                    }
-                });
-        }
-    });
+	workspace.onDidChangeConfiguration((e: ConfigurationChangeEvent) => {
+		if (e.affectsConfiguration('v.vls.enable')) {
+			if (isVlsEnabled()) {
+				void activateVls();
+			} else {
+				deactivateVls();
+			}
+		} else if (e.affectsConfiguration('v.vls') && isVlsEnabled()) {
+			void vscode.window.showInformationMessage('VLS: Restart is required for changes to take effect. Would you like to proceed?', 'Yes', 'No')
+				.then(selected => {
+					if (selected == 'Yes') {
+						void vscode.commands.executeCommand('v.vls.restart');
+					}
+				});
+		}
+	});
 
-    const shouldEnableVls = isVlsEnabled();
-    if (shouldEnableVls) {
-        void activateVls();
-    }
+	const shouldEnableVls = isVlsEnabled();
+	if (shouldEnableVls) {
+		void activateVls();
+	}
 }
 
 export function deactivate(): void {
-    if (isVlsEnabled()) {
-        deactivateVls();
-    }
+	if (isVlsEnabled()) {
+		deactivateVls();
+	}
 }
--- src/tasks.ts
+++ src/tasks.ts
@@ -2,57 +2,57 @@ import * as vscode from "vscode";
 
 // Basically just a map to an invocation of V
 export interface VTask extends vscode.TaskDefinition {
-    /**
-     * The V subcommand to use (blank for compile)
-     */
-    command: string;
-    /**
-     * The arguments to pass to the subcommand
-     */
-    args: string[];
-    /**
-     * The file or folder to compile
-     */
-    target: string;
+	/**
+	 * The V subcommand to use (blank for compile)
+	 */
+	command: string;
+	/**
+	 * The arguments to pass to the subcommand
+	 */
+	args: string[];
+	/**
+	 * The file or folder to compile
+	 */
+	target: string;
 }
 
 
 function makeTask(task: string, args?: string[]): vscode.Task {
-    return new vscode.Task(
-        {
-            type: "v",
-            task,
-            args,
-        },
-        vscode.TaskScope.Workspace,
-        task,
-        "V",
-        new vscode.ShellExecution(`v ${task == "build" ? "" : task} ${args ?? ''} .`),
-        "$gcc"
-    );
+	return new vscode.Task(
+		{
+			type: "v",
+			task,
+			args,
+		},
+		vscode.TaskScope.Workspace,
+		task,
+		"V",
+		new vscode.ShellExecution(`v ${task == "build" ? "" : task} ${args ?? ''} .`),
+		"$gcc"
+	);
 }
 
 export class VTaskProvider implements vscode.TaskProvider {
-    public provideTasks(): vscode.Task[] {
-        let build = makeTask("build");
-        build.group = vscode.TaskGroup.Build;
-        let run = makeTask("run");
-        run.group = vscode.TaskGroup.Test;
-        let test = makeTask("test");
-        test.group = vscode.TaskGroup.Test;
-        return [
-            run,
-            build,
-            test,
-        ];
-    }
+	public provideTasks(): vscode.Task[] {
+		let build = makeTask("build");
+		build.group = vscode.TaskGroup.Build;
+		let run = makeTask("run");
+		run.group = vscode.TaskGroup.Test;
+		let test = makeTask("test");
+		test.group = vscode.TaskGroup.Test;
+		return [
+			run,
+			build,
+			test,
+		];
+	}
 
-    public resolveTask(task): vscode.Task | undefined {
-        if (task.definition.task) {
-            const t: VTask = <any>task.definition;
-            var new_task = makeTask(t.command, t.args);
-            new_task.execution = new vscode.ShellExecution(`v ${t.command} ${t.args ?? ''} .`);
-        }
-        return undefined;
-    }
+	public resolveTask(task): vscode.Task | undefined {
+		if (task.definition.task) {
+			const t: VTask = <any>task.definition;
+			var new_task = makeTask(t.command, t.args);
+			new_task.execution = new vscode.ShellExecution(`v ${t.command} ${t.args ?? ''} .`);
+		}
+		return undefined;
+	}
 }
